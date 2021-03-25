4b5417e6c70115049883300ebaab73777e0e7075
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-25 18:05:23 +0900

    added some side bars

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -27,12 +27,23 @@ class Map extends React.Component {
     center: [lng, lat],
     zoom: zoom
     });
+
+    map.on('move', () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
   }
 
   render() {
    const { lng, lat, zoom } = this.state;
    return (
      <div>
+       <div className="sidebar">
+         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+       </div>
        <div ref={this.mapContainer} className="map-container" />
      </div>
    );
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Sidebar extends React.Component {
+  render() {
+    const { lng, lat, zoom } = this.state;
+    return (
+      <div>
+        <div className="sidebar">
+          Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+        </div>
+        <div ref={this.mapContainer} className="map-container" />
+      </div>
+    );
+  }
+}
