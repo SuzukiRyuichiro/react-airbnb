fc16de1fafc0144c0b03156348d11cd6df26f626
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-03-26 17:23:22 +0900

    changed the name for flats

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -21,7 +21,7 @@ class Flat extends Component {
       <div className="card" id={this.props.id} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.state.imageUrl})`}} >
         <div className="card-category">{this.state.price} {this.state.priceCurrency}</div>
         <div className="card-description">
-          <h2>Super 60m2 in trendy neighborhood!</h2>
+          <h2>{this.state.name}</h2>
         </div>
       </div>
     );
