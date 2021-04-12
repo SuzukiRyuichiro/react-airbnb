d4682f39482290258c902a27441babbe2b457457
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-12 20:53:04 +0900

    removed states from flats cuz it was unnecessary

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -18,10 +18,10 @@ class Flat extends Component {
 
   render () {
     return (
-      <div className="card" id={this.props.id} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.state.imageUrl})`}} >
-        <div className="card-category">{this.state.price} {this.state.priceCurrency}</div>
+      <div className="card" id={this.props.id} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.props.flat.imageUrl})`}} >
+        <div className="card-category">{this.props.flat.price} {this.props.flat.priceCurrency}</div>
         <div className="card-description">
-          <h2>{this.state.name}</h2>
+          <h2>{this.props.flat.name}</h2>
         </div>
       </div>
     );
